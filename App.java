46ee69a9e85134a7cc659210ae416911d79c86dd
dilenpt <107061918+dilenpt@users.noreply.github.com>
2022-06-09 15:41:57 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,23 @@
+
+
+public class App {
+    public static void main(String[] args)  {
+       String name = "kevin" ;
+       int age=22;
+
+       hello(name,age);
+       
+    }
+        static void hello(String name, int age){
+            System.out.println("hello "+name);
+            System.out.println("you are"+age+"years old");
+        }
+    }
+    
+
+
+
+
+
+
+
